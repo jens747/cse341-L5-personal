258002aa8221507a1c52893b2c1b64dbe4bab16f
Joel <jen21092@byui.edu>
2024-11-09 20:25:35 -0700

    reverting to full swagger.json

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 // Import the Swagger Express module
 const swaggerUi = require("swagger-ui-express");
 // Import Swagger JSON module
-// const swaggerDocument = require("../swagger.json");
+const swaggerDocument = require("../swagger.json");
 const swaggerSpec = require("../config/swaggerConfig");
 
 // Set up Swagger Express API
-// router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = router;
